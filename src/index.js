0c413b461f434260d194a4dddf67daf7ac585f06
igorvaz-pravaler <igor.vaz@pravaler.com.br>
2023-05-26 19:27:42 -0300

    feat: create users endpooint

--- src/index.js
+++ src/index.js
@@ -1,21 +1,40 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  
+  const { name, username } = request.body;
+
+  const userAlreadyExists = users.some(
+    (user)=> user.username === username
+  )
+
+  if (userAlreadyExists){
+    return response.status(400).json({error: "User already exists!"});
+  }
+
+  users.push({
+    id: uuidv4(), 
+    name,
+    username,
+    todos: []
+  });
+
+  return response.status(201).json(users);
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
